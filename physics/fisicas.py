6b7f3ccc720a34d0fa58d3f4875504de9b4e5f00
JuanJo-02 <juan.rivera22@uptc.edu.co>
2026-05-14 17:54:24 -0500

    añadiendo clase fisicas

---
+++ physics/fisicas.py
@@ -0,0 +1,11 @@
+import math
+
+import constantes
+
+angulo_rad = math.radians(constantes.ANGULO)
+
+velocidad_x = (constantes.VELOCIDAD_INICIAL*math.cos(constantes.ANGULO))
+velocidad_y = (constantes.VELOCIDAD_INICIAL*math.sin(constantes.ANGULO))
+
+#dibujar_texto (f"Velocidad Inicial : {constantes.VELOCIDAD_INICIAL} m/s"),
+                #constantes.info,constantes.COLOR_NEGRO,20,140)
