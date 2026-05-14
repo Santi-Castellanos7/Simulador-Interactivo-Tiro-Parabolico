15dd5f39f7d55cbce4767823b5b1a76b0fdf6824
Santi Ago <santiagoalfonso5674@gmail.com>
2026-05-14 15:34:35 -0500

    Agregué un hola

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ class main:
     print("hola")
     print("Amigos")
     print("Friendly")
-<<<<<<< HEAD
     print ("Los quiero")
-=======
     print("Papus")
->>>>>>> 9d4320dfd3a06d8476cc18fb9d197126ab2d313d
+    print("hola")
