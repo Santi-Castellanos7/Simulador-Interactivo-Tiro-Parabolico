ece8a969933306471e7bf9f605a0d5e3acb96ced
Santi Ago <santiagoalfonso5674@gmail.com>
2026-05-14 17:43:35 -0500

    Agregué las carpetas para separar la ui, las constantes y el main para mayor organización y también cambié el nombre de la clase a inglés

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import pygame
 import math
 
-import constantes
+import constants
 
 #Inicializar la libreria de pygame
 pygame.init()
 
 #Crear ventana del juego
 
-ventana = pygame.display.set_mode(constantes.ALTO_VENTANA, constantes.ANCHO_VENTANA)
+ventana = pygame.display.set_mode(constants.ALTO_VENTANA, constants.ANCHO_VENTANA)
+
+#Ponerle nombre a la ventana
+
+pygame.display.set_caption("Simulador interactivo de tiro parabólico")
+
+
+running = True
+clock = pygame.time.Clock()
+
+#
+while running:
+
+    clock.tick(constants.FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
 
 
 
